730d3d6a3b418e444b65558725a44b2de734de83
Tx3ThanhTung <basketball10012004@gmail.com>
2025-11-05 23:28:33 +0700

    Update script.js

--- Hpbd1/script.js
+++ Hpbd1/script.js
@@ -611,9 +611,9 @@ scene.add(planet);
 
 // ---- TẠO CÁC VÒNG CHỮ QUAY QUANH HÀNH TINH ----
 const ringTexts = [
-  'XIN LỖI VÌ NHỮNG LẦN LÀM EM BUỒN',
-  "THƯƠNG MỖI EM NGUYÊN",
-  "TIENDEV",
+  'Anh iu Linhhh',
+  "Happy Birthday nhóoo",
+  "Yêu Linhhhhh",
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
@@ -1323,3 +1323,4 @@ window.addEventListener('orientationchange', () => {
   // Thêm độ trễ để trình duyệt cập nhật kích thước chính xác
   setTimeout(checkOrientation, 200);
 });
+
